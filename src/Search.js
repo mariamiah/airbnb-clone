d3ff786be2977172d48649c3fdc92e147c8f0e90
mariamiah <mariah.kakande@gmail.com>
2020-11-05 23:08:18 +0300

    add search functionality

--- src/Search.js
+++ src/Search.js
@@ -3,6 +3,8 @@ import './Search.css'
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "react-date-range";
+import { Button } from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/People';
 
 
 //DATE PICKER COMPONENT
@@ -24,6 +26,12 @@ function Search() {
             <DateRangePicker ranges={
                 [selectionRange]
             } onChange={handleSelect} />
+            <h2>
+                Number of guests
+                <PeopleIcon />
+            </h2>
+            <input min={0} defaultValue={2} type="number" />
+            <Button>Search Airbnb</Button>
         </div>
     )
 }
