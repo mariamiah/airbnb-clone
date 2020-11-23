affb46a4624bfcb167a7a7bed2f3a927122d9d23
mariamiah <mariah.kakande@gmail.com>
2020-11-23 10:15:22 +0300

    setup footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
+        <div className="footer">
+            <p>2020 Airbnb clone! No rights reserved - this
+                is a demo!
+            </p>
+            <p>
+                Privacy . Terms . Sitemap . Company Details
+            </p>
             
         </div>
     )
