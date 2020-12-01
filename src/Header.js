301ccee52faa3407d7d66446ad80869c8fa00911
mariamiah <mariah.kakande@gmail.com>
2020-12-01 09:48:20 +0300

    add routing

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,13 @@ import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
 import LanguageIcon from '@material-ui/icons/LanguageOutlined';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMoreOutlined';
+import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="header">
+            <Link to='/'>
             <img className="header__icon" src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" alt="" />
+            </Link>
             <div className="header__center">
              <input type="text" />
              <SearchIcon />
